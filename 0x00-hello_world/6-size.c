98a65fd97dfda234542930949b4bb8d452650229
abdulrahman <abdulrahmanheiba253@gmail.com>
2023-09-07 14:55:10 -0700

    c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: "sizeof(char));
+	printf("Size of a int: "sizeof(int));
+	printf("Size of a long int: "sizeof(long int));
+	printf("Size of a long long: "sizeof(long long));
+	return (0)
+}
