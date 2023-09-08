417b865b611c46dd5a4c620ffbfdb942a5f37ee9
abdulrahman <abdulrahmanheiba253@gmail.com>
2023-09-08 14:43:23 -0700

    ifelse

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet.
+ * main - Print the  alphabet.
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
-        int i ;
+	int i;
 
-	for (i = 0; i <26; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alpha[i]);
 	}
