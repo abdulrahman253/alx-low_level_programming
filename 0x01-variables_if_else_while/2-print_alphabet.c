5fe971f33d80bc96654064b44dc484d8cc1f0238
abdulrahman <abdulrahmanheiba253@gmail.com>
2023-09-08 14:40:40 -0700

    ifelse

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,9 +11,9 @@ int main(void)
 	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
         int i ;
 
-	for (i=0 , i<26 , i++)
+	for (i = 0 , i <26 , i++)
 	{
-		putchar(alp[i]);
+		putchar(alpha[i]);
 	}
 	putchar('\n');
 	return (0);
