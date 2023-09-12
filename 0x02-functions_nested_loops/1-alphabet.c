32b710eacbb137f54e211e6b6b073acaeada2d02
abdulrahman <abdulrahmanheiba253@gmail.com>
2023-09-12 05:10:19 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+include "main.h"
+/**
+ * main - print_alphabet
+ * Return 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+	while (char c <= z)
+	{
+		_putchar(c);
+		c++;	
+	}
+	_putchar('\n');
+
+}
+
