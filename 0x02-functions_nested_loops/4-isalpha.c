cbc2d53d5ed39115637abb2ec3c3665586fd31de
abdulrahman <abdulrahmanheiba253@gmail.com>
2023-09-12 09:52:05 -0700

    islower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * _isalpha - Checks for alphabetic character
+  * @c: The character to be checked
+  *
+  * Return: 1 for alphabetic character or 0 for anything else
+  */
+int print_sign(int n)
+{
+	if ((n >= 97 && c <= 122) || (n >=65 && c <= 90))
+	{
+		return (1);
+	}
+	return (0);
+}
