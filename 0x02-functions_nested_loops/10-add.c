a134cbd8a1d5997fe46b11a91b90507adb42609f
abdulrahman <abdulrahmanheiba253@gmail.com>
2023-09-13 04:01:35 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
   * add - Adds two values
-  * @n1: The first number to be computed
-  * @n2: The second number to be computed
+  * @a: The first number to be computed
+  * @b: The second number to be computed
   *
   * Return: Integer value
   */
