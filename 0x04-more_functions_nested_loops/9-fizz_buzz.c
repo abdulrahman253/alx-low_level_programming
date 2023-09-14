28aedc489d79600190bcda38ce7017c2462ad9a1
abdulrahman <abdulrahmanheiba253@gmail.com>
2023-09-14 15:57:07 -0700

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,14 +5,13 @@
   *
   * Return: Always 0 (Success)
   */
-
 int main(void)
 {
 	int n;
 	
 	for (n = 1; n <= 100; n++)
 	{
-       		 if ((n % 3 == 0) && (n % 5 == 0 ))
+       		 if ((n % 3 == 0) && (n % 5 == 0))
 		 {
 			 printf("fizzbuzz");
 		 }
