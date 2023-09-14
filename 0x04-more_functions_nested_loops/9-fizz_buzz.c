4b58ac76e65ea78140f7902a577c344846876e96
abdulrahman <abdulrahmanheiba253@gmail.com>
2023-09-14 15:59:11 -0700

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,26 +8,32 @@
 int main(void)
 {
 	int n;
-	
+
 	for (n = 1; n <= 100; n++)
 	{
-       		 if ((n % 3 == 0) && (n % 5 == 0))
-		 {
-			 printf("fizzbuzz");
-		 }
-		 else if (n % 3 == 0)
-		 {
-			 printf("Fizz");
-		 }
-		 else if (n % 5 == 0)
-		 {
-			 printf("buzz");
-		 }
-		 else
-		 {
-			 printf(" %d ", n);
-		 }
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+
+		if (x != 100)
+		{
+			printf(" ");
+		}
 	}
+
 	printf("\n");
 	return (0);
 }
