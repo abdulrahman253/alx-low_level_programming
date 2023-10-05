900446a397b3a514e830c3892bedb02e3e07bf9f
abdulrahman253 <abdulrahmanheiba253@gmail.com>
2023-10-05 12:58:44 -0700

    maloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,7 +7,7 @@
  * @size: size of the array to create
  * @c: char to initialize the array c
  *
- * Return: pointer to the array (Success), NULL (Error)
+ * Return: pointer to the array if (Success), NULL if (Error)
  */
 char *create_array(unsigned int size, char c)
 {
@@ -31,3 +31,4 @@ char *create_array(unsigned int size, char c)
 	*(p + i) = '\0';
 
 	return (p);
+}	
